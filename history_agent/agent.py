18d98d5e7c3a94f7da6416f6ffa1e2d6033ae064
Your Nameshinderohit696-cmyk <shinderohit696@gmail.com>
2026-05-14 22:09:52 +0530

    added changes in history_agent

--- history_agent/agent.py
+++ history_agent/agent.py
@@ -2,8 +2,8 @@ from google.adk.agents import Agent
 
 root_agent = Agent(
     name="greeting_agent", # name of the agent
-    model="gemini-2.5-flash-lite", # LLM provider
-    description="Greeting agent", # description provided to agent
+    model="gemini-2.5-flash-lite", # LLM provider By gemini
+    description="Greeting agent", # description provided to agent 
     instruction="""
     You are a helpful assistant that provide the Information on Indian history only. 
     provide indian history related answers only .
